c0bc29b2019e599cc4bcbf7e8c6503083fc2a44e
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-02-19 22:58:34 +0700

    find_min_num update

--- method.py
+++ method.py
@@ -1,7 +1,22 @@
 # Tìm số nhỏ nhất
 def find_min_num(arr_num):
     min_num = arr_num[0]
-    for num in arr_num:
-        if num < min_num:
-            min_num = num
+    # Duyệt 1 chiều
+    # for num in arr_num:
+    #     if num < min_num:
+    #         min_num = num
+    
+    # index = 0
+    # while index < len(arr_num):
+    #     if arr_num[index] < min_num:
+    #         min_num = arr_num[index]
+    #     index += 1
+    
+    # index = len(arr_num) - 1
+    # while index > 0:
+    #     if arr_num[index] < min_num:
+    #         min_num = arr_num[index]
+    #     index -= 1
+    
+    # Duyệt 2 chiều
     return min_num
